da5a741f84f9e30017eab3a672205eb4ccef50ea
waleplanet <walexrealest@gmail.com>
2017-09-28 03:35:12 +0100

    add token login function

--- app/src/main/java/com/app/waleent/pizzaapp/LoginActivity.java
+++ app/src/main/java/com/app/waleent/pizzaapp/LoginActivity.java
@@ -4,19 +4,15 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
-
-import android.support.v7.app.AppCompatActivity;
-
-
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,31 +30,8 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-
-/**
- * A login screen that offers login via email/password.
- * implements LoaderCallbacks<Cursor>
- */
 public class LoginActivity extends AppCompatActivity {
-    private static final String TAG ="" ;
-
-    /**
-     * Id to identity READ_CONTACTS permission request.
-    private static final int REQUEST_READ_CONTACTS = 0;
-     */
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-     */
-
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-//    private UserLoginTask mAuthTask = null;
+    private static final String TAG = "";
 
     private SessionManager session;
     // UI references.
@@ -67,14 +40,14 @@ public class LoginActivity extends AppCompatActivity {
     private View mProgressView;
     private View mLoginFormView;
     private Button mEmailSignInButton;
-    public static final String BASE_URL = "https://pizzaappbackend.herokuapp.com/";
+    public static final String BASE_URL = "https://pizzaappbackend.herokuapp.com/api-jwt-auth/";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mEmailView =  findViewById(R.id.email);
+        mEmailView = findViewById(R.id.email);
 
         mPasswordView = findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -104,24 +77,17 @@ public class LoginActivity extends AppCompatActivity {
 
         // Check if user is already logged in or not
         if (session.isLoggedIn()) {
-            // User is already logged in. Take him to main activity
+            Toast.makeText(getApplicationContext(),"Loged In",Toast.LENGTH_SHORT);
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
         }
     }
 
-
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
     private void attemptLogin() {
         if (!mEmailSignInButton.isEnabled()) {
             return;
         }
-
         // Reset errors.
         mEmailView.setError(null);
         mPasswordView.setError(null);
@@ -157,14 +123,8 @@ public class LoginActivity extends AppCompatActivity {
             // form field with an error.
             focusView.requestFocus();
         } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-//            UserLoginTask loginTask= new UserLoginTask(email,password);
-//            loginTask.execute();
             checkLogin(email, password);
             showProgress(true);
-           /* mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);*/
         }
     }
 
@@ -215,30 +175,33 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private void checkLogin(final String username,final String password){
-        String tag_string_req= "login str";
+    private void checkLogin(final String username, final String password) {
+        String tag_string_req = "login request";
+        showProgress(true);
+
         StringRequest strReq = new StringRequest(Request.Method.POST,
                 BASE_URL, new Response.Listener<String>() {
-
-
             @Override
             public void onResponse(String response) {
                 Log.d(TAG, "Login Response: " + response.toString());
-
+                showProgress(false);
+                boolean error=false;
+                String errorMsg="";
                 try {
                     JSONObject jObj = new JSONObject(response);
-                    boolean error = jObj.getBoolean("error");
+
+                    if(jObj.has("non_field_errors")){
+                        error=true;
+                        errorMsg = jObj.getString("non_field_errors");
+                    }
 
                     // Check for error node in json
                     if (!error) {
                         // user successfully logged in
                         // Create login session
                         session.setLogin(true);
-
                         // Now store the user in SQLite
                         String token = jObj.getString("token");
-
-
                         // Launch main activity
                         Intent intent = new Intent(LoginActivity.this,
                                 MainActivity.class);
@@ -246,7 +209,6 @@ public class LoginActivity extends AppCompatActivity {
                         finish();
                     } else {
                         // Error in login. Get the error message
-                        String errorMsg = jObj.getString("non_field_errors");
                         Toast.makeText(getApplicationContext(),
                                 errorMsg, Toast.LENGTH_LONG).show();
                     }
@@ -264,6 +226,7 @@ public class LoginActivity extends AppCompatActivity {
                 Log.e(TAG, "Login Error: " + error.getMessage());
                 Toast.makeText(getApplicationContext(),
                         error.getMessage(), Toast.LENGTH_LONG).show();
+                showProgress(false);
             }
         }) {
 
@@ -273,14 +236,11 @@ public class LoginActivity extends AppCompatActivity {
                 Map<String, String> params = new HashMap<String, String>();
                 params.put("username", username);
                 params.put("password", password);
-
                 return params;
             }
-
         };
         // Adding request to request queue
         AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
     }
-
 }
 
